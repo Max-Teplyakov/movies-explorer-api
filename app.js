25f4702a2c357fe495f11d867ae713c74af88a61
Max Teplyakov <maxxtepll@gmail.comgit config --listgit config --global user.email maxxtepll@gmail.com>
2023-07-25 14:44:04 +0400

    добавмил env

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 const routes = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/filmsdb' } = process.env;
+const { PORT = 3000, DB_URL } = process.env;
 app.use(cors());
 
 app.use(express.json());
--- controllers/users.js
+++ controllers/users.js
@@ -10,11 +10,13 @@ const {
   OK_SERVER,
 } = require('../utils/utils');
 
+const { NODE_ENV, JWT_SECRET } = process.env;
+
 module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
   User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, 'super-strong-secret', { expiresIn: '7d' });
+      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
       res.send({ token });
     })
     .catch(next);
